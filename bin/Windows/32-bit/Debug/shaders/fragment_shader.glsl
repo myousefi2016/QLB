38bc488fe5c6519cee861501b1b86677860293d3
thfabian <fabian_thuering@hotmail.com>
2015-02-15 16:54:13 +0100

    Update repo

--- bin/Windows/32-bit/Debug/shaders/fragment_shader.glsl
+++
@@ -1,6 +0,0 @@
-varying vec4 vColor;
-
-void main (void) 
-{ 
-   gl_FragColor = vColor; 
-}
--- bin/Windows/64-bit/shaders/vertex_shader.glsl
+++
@@ -1,7 +0,0 @@
-varying vec4 vColor;
-
-void main(void)
-{
-   vColor = gl_Color;
-   gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-}
